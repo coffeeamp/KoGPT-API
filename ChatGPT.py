ec5a26ead5ccc3434784d408066507abc9b9636f
hjonghyeok <whdgur1999@gmail.com>
2023-04-18 15:06:21 +0900

    kogpt 파이썬 코드 연동완료

--- ChatGPT.py
+++ ChatGPT.py
@@ -1,16 +1,18 @@
 from PyKakao import KoGPT
 
 # KoGPT API 인스턴스 생성
-GPT = KoGPT(service_key = "94c513ac26370fe7d8e48c4a3c8baeec")
+GPT = KoGPT(service_key = "d8fc586c23874c245e239eac03985e25")
 
-# 문장을 input()을 활용하여 입력받기
-prompt = input("문장을 입력하세요: ")
-max_tokens = 128
-result = GPT.generate(prompt, max_tokens, temperature=0.7, top_p=0.8)
+def chatbot(prompt):
+    # 문장을 input()을 활용하여 입력받기
+    max_tokens = 128
+    result = GPT.generate(prompt, max_tokens, temperature=0.7, top_p=0.8)
 
 
-# 문장이 끝이 안나면 이전 .까지 자르고 다시 .을 붙여서 출력
-if result['generations'][0]['text'].split(".")[-1] != "":
-    print(result['generations'][0]['text'].split(".")[0] + '.')
-else:
-    print(result['generations'][0]['text'])
+    # 문장이 끝이 안나면 이전 .까지 자르고 다시 .을 붙여서 출력
+    if result['generations'][0]['text'].split(".")[-1] != "":
+        print(result['generations'][0]['text'].split(".")[0] + '.')
+        return result['generations'][0]['text'].split(".")[0] + '.'
+    else:
+        print(result['generations'][0]['text'])
+        return result['generations'][0]['text']
--- chat/views.py
+++ chat/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from PyKakao import KoGPT
 
+from ChatGPT import chatbot
+
 # Create your views here.
 def home(request):
-   answer = "abcd"
+   answer = chatbot("안녕하세요")
    return render(request, 'Chatfile.html', {'answer': answer})
 
    
